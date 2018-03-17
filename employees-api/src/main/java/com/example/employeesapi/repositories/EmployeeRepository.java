4ccc0467b38be0163fb9b8a437b17cf7eba9cb04
jmbothe <jeffrymbothe@gmail.com>
2018-03-17 10:03:30 -0400

    setup employee crud repo

---
+++ employees-api/src/main/java/com/example/employeesapi/repositories/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.example.employeesapi.repositories;
+
+import com.example.employeesapi.models.Employee;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository <Employee, Long> {
+}
